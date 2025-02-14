23d8d5099e30ecd99eb7f3455fccac7123df1dd2
Rishabh Umredkar <umredkarrishabh@gmail.com>
2025-02-14 17:51:18 +0530

    Configuration Using Annotation

---
+++ BeanFactory/src/main/java/ConfigAnnotation/Bank.java
@@ -0,0 +1,11 @@
+package ConfigAnnotation;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "myBank")
+public class Bank {
+	public void getloan()
+	{
+		System.out.println("Scam.......");
+	}
+}
