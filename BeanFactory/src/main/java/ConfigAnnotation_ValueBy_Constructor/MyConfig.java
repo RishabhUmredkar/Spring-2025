f0d61821085fe7b3912c5896fee0513ec0d7db2e
Rishabh Umredkar <umredkarrishabh@gmail.com>
2025-02-14 18:11:28 +0530

    value by Setter by annotation

---
+++ BeanFactory/src/main/java/ConfigAnnotation_ValueBy_Constructor/MyConfig.java
@@ -0,0 +1,10 @@
+package ConfigAnnotation_ValueBy_Constructor;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ConfigAnnotation_ValueBy_Constructor")
+public class MyConfig {
+
+}
