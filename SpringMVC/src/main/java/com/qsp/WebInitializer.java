c386c50ad74ef62fb2336ea0c122dd04f9e72be0
Rishabh Umredkar <umredkarrishabh@gmail.com>
2025-02-23 01:57:08 +0530

    Spring MVC

---
+++ SpringMVC/src/main/java/com/qsp/WebInitializer.java
@@ -0,0 +1,24 @@
+package com.qsp;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {MyConfig.class};
+		
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
